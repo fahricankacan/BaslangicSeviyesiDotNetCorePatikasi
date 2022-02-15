3d587470c5d8246b546a79b6f7063e75198336bf
Fahrican Kaçan <fahrican.kcn@gmail.com>
2022-02-15 23:27:01 +0300

    Diziler1 eklendi.

---
+++ diziler1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace diziler1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //diziler1
+            string[] renkler = new string[5];
+
+            string[] hayvanlar = { "Kediler", "Köpekler", "Kuş", "Maymun" };
+
+            int[] dizi;
+            dizi = new int[5];
+
+            renkler[0] = "Mavi";
+            dizi[3] = 10;
+
+            System.Console.WriteLine(hayvanlar[1]);
+            System.Console.WriteLine(dizi[3]);
+            System.Console.WriteLine(renkler[0]);
+
+            System.Console.WriteLine("Lütgen dizinn eleman sayisini giriniz : ");
+            int diziUzulugu = int.Parse(Console.ReadLine());
+            int[] sayiDizisi = new int[diziUzulugu];
+
+            for (int i = 0; i < diziUzulugu; i++)
+            {
+                System.Console.WriteLine("Lütfen {0}, sayısı giriniz", i + 1);
+                sayiDizisi[i] = int.Parse(Console.ReadLine());
+            }
+
+            int toplam = 0;
+            foreach (var sayi in sayiDizisi)
+            {
+                toplam += sayi;
+            }
+            System.Console.WriteLine("Ortalama : " + toplam / diziUzulugu);
+        }
+    }
+}
