8a74d25ae3c4f4d92e2d1c0b5896ee93b54b6778
Fahrican Kaçan <fahrican.kcn@gmail.com>
2022-02-18 01:38:55 +0300

    Array List eklendi.

---
+++ arrayList/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace arrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList listele = new();
+            listele.Add("Ayşe");
+            listele.Add(2);
+            listele.Add(true);
+            listele.Add('A');
+
+            System.Console.WriteLine(listele[1]);
+
+            foreach (var item in listele)
+                System.Console.WriteLine(item);
+
+            System.Console.WriteLine("******Add Range *******");
+            string[] renkler = { "kırmızı", "sari", "pembe" };
+            List<int> sayılar = new() { 1, 2, 3, 4, 51, 321, 5, 123 };
+            listele.AddRange(renkler);
+            listele.AddRange(sayılar);
+
+            foreach (var item in listele)
+                System.Console.WriteLine(item);
+
+
+            System.Console.WriteLine("**** Sort *****");
+            sayılar.Sort();//array list içersinde 2 farklı ICollection olduğu için sıralamaz hata verir
+            foreach (var item in sayılar)
+                System.Console.WriteLine(item);
+
+            System.Console.WriteLine("********Binary Search ********");
+            System.Console.WriteLine(sayılar.BinarySearch(51));
+
+            //reverse
+            System.Console.WriteLine("****Reverse ****");
+            listele.Reverse();
+            foreach (var item in listele)
+                System.Console.WriteLine(item);
+
+            //clear
+            System.Console.WriteLine("**** Clear ****");
+            listele.Clear();
+
+            foreach (var item in listele)
+                System.Console.WriteLine(item);
+        }
+    }
+}
